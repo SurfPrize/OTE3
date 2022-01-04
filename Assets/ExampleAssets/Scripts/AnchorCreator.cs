4dbf2c4b491e0c7955b1cb6dbca4e35ec8f01b38
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-04 22:24:04 +0000

    touchscreen joystick

--- Assets/ExampleAssets/Scripts/AnchorCreator.cs
+++ Assets/ExampleAssets/Scripts/AnchorCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:933f851ba000ed93d0e50018716ef31bb3957d8b0f5a8e1cf24b30fa51000eb7
-size 5116
+oid sha256:9005b8a4f739e463034c5ca1b70081ec02fd732e8225b352e41016f2c7b4aa7c
+size 5117
--- Assets/Scripts/PlayerSystem/PlayerSystem.cs
+++ Assets/Scripts/PlayerSystem/PlayerSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:940d47cb8a8155eb1d2680314a7117182659c45cf72e5b7dea940b549ddb0b25
-size 611
+oid sha256:70f1ca83fab64f261b2df75a81c366ad5da6c63253e7c14e3b2bc8af28d9d740
+size 986
