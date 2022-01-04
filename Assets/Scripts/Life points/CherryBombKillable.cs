045f3d0fb497dcaa42ac165244aff4fc5dbd2f9f
Dragvid <55921056+Dragvid@users.noreply.github.com>
2022-01-04 18:43:14 +0000

    cherry bomb enemy pt 2

--- Assets/Scripts/Life points/CherryBombKillable.cs
+++ Assets/Scripts/Life points/CherryBombKillable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:062d5bed8293b66c18046c5775370bc5aa0ce1aaa15be0ab58d182d07365bb4d
-size 302
+oid sha256:a2e6463d46491f368ee1014a076b4c9dde45e248e082ba1dd1734f844351a1e4
+size 455
--- Assets/Scripts/Life points/Killable.cs
+++ Assets/Scripts/Life points/Killable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b17d9f9bca56d05d83205d5464c6ef5a9b037113782698b48aa02e069b9af38
-size 490
+oid sha256:01278f3407b18b917f7dc1b65f6e1d222a9ccc09ad6522790786eec8e988ab5c
+size 528
