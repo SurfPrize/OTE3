4a1d1fc82361b2438218714f0225d33882a6651f
Dragvid <55921056+Dragvid@users.noreply.github.com>
2022-01-06 12:58:12 +0000

    lock on and attack system

--- Assets/Scripts/PlayerSystem/LockOnSystem.cs
+++ Assets/Scripts/PlayerSystem/LockOnSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18ce86842add75035b46348f1c4276ae6d2586b065e8ff2059073825a8a07620
-size 896
+oid sha256:94f6551ca6ad329684ab255b15cc255a3711633ee65f85deadec6ec8e911ab46
+size 3236
