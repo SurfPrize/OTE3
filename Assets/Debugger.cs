e152d300b57f5a5a693fdd11d0ec6484e950b848
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-07 16:52:43 +0000

    player movement Beta

--- Assets/Debugger.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8ab0a969f02bb06f4f329f16eb427d5a866e153eb2bb18c50621b17d3aeae41a
-size 729
--- Assets/Scripts/PlayerSystem/PlayerSystem.cs
+++ Assets/Scripts/PlayerSystem/PlayerSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11d753c6ce15564c057c596b0cdede8fd967828657d5f55a64a361e92f50c87f
-size 2414
+oid sha256:e515218125fa7794b20a112f9e21fb1e29b41076335aea9d0921b706b362bf87
+size 3022
