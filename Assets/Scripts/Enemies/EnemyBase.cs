fefcabf619df77c0547337416dacb58a477eb3e4
Dragvid <55921056+Dragvid@users.noreply.github.com>
2022-01-08 12:04:36 +0000

    enemy and spawner prefabs

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a7baf365c2a4e85ec016f55a52a413a381fba0e909f136240bda99f64fa4b6e
-size 3042
+oid sha256:23ccdb122470b9a83952573be298a195453883892627c298d18a5eb2b280ede0
+size 2948
