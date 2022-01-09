f769d92d3b7e9216e56ad75bde8d3a911e53986c
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-09 22:35:22 +0000

    Revert "Merge branch 'main' of https://github.com/SurfPrize/OTE3"
    This reverts commit 15dd337467dd913cca2ec32d42462cd616390b9b, reversing
    changes made to 19d77c84493ced049b7bd03cb0ffd28cd22f1ac6.

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df7d943afea276635995850a45ca78827770cee551b7207aee4d771c874ac0d6
-size 1540
+oid sha256:c9f523222e0ab4299d2506d9dfae5b1df8b1ccbf0118d6ed9577cb547164debb
+size 1554
