41ea832ccdb916431325c59ea70796fe51111394
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-09 21:16:24 +0000

    organizacao e fix

--- Assets/Scripts/Spawner/spawner/EnemySpawner.cs
+++ Assets/Scripts/Spawner/spawner/EnemySpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9ebab07b99570833c7f9ddbed28856982618e18bb8b6a458047b846e966a67
-size 4858
+oid sha256:a47e45b82fe5293ec07275760d8af0172e37d51804b6a6b58a18e5d9cf9de273
+size 4860
