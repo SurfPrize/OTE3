cbc6dec1a40f0cdfaea5fe1b0bbe3dd314967d4e
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-09 22:10:44 +0000

    fixes

--- Assets/Scripts/Life points/Killable.cs
+++ Assets/Scripts/Life points/Killable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bd9e104efb0cd86a723cb5749627e619488a4eadabefd7608498d4e1e1d7bc4
-size 1291
+oid sha256:e64f947cfa705e4f0c87d439631477a8c642630379fa65194b0062cc3f3b47e4
+size 1300
