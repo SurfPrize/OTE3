cae814e14b2c456939ef878032af8185ef11d155
Dragvid <55921056+Dragvid@users.noreply.github.com>
2022-01-09 17:27:24 +0000

    Spawner update

--- Assets/Scripts/Spawner/spawner/EnemySpawner.cs
+++ Assets/Scripts/Spawner/spawner/EnemySpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:617117f84a077dee5440873bb1e2c3b311dad84cab0ea4664d3734c9fb03d3f5
-size 4572
+oid sha256:4b9ebab07b99570833c7f9ddbed28856982618e18bb8b6a458047b846e966a67
+size 4858
