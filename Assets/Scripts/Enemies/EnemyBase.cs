85a23c7b766fbc4a4e9f01d41ca73445f38a0e65
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2022-01-09 22:14:38 +0000

    Update EnemyBase.cs

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ddb8d68fae12f534c7a011f404c5a40b479cd3da9ed45361521ccc36443e274
-size 3244
+oid sha256:cae6549cc77d50ee0f560ea5207ad2ebf69412479c6925e666de80821ee34e92
+size 2827
